323d402f84d5955970caf87f1b9bc387cea3af5e
Tagir Magomedov <tmagomedov@twitter.com>
2015-06-25 17:04:33 -0700

    Simple remoting

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "My Project"
+ 
+version := "1.0"
+ 
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-actor" % "2.3.11",
+  "com.typesafe.akka" %% "akka-remote" % "2.3.11"
+)
+
---
+++ src/main/scala/info/fotm/IntroApp.scala
@@ -0,0 +1,70 @@
+package info.fotm
+
+import akka.actor._
+import akka.actor.Actor.Receive
+import akka.event.LoggingReceive
+
+object Lobby {
+  case object Start
+  case class Connect(client: ActorRef)
+  case class Connected(client: ActorRef)
+  case class Disconnect(client: ActorRef)
+  case class Disconnected(client: ActorRef)
+}
+
+class Lobby extends Actor {
+  def receive: Receive = LoggingReceive {
+    case Lobby.Start =>
+      context.become(work(Nil))
+  }
+
+  def work(connections: List[ActorRef]): Receive = LoggingReceive {
+    case Lobby.Connect(client) =>
+      println(s"$client connected")
+      val updatedConnections = client :: connections
+      context.become(work(updatedConnections))
+      updatedConnections.foreach { _ ! Lobby.Connected(client) }
+
+    case Lobby.Disconnect(client) =>
+      println(s"$client disconnected")
+      val updatedConnections = connections diff List(client)
+      context.become(work(updatedConnections))
+      connections.foreach {_ ! Lobby.Disconnected(client) }
+  }
+}
+
+class LobbyClient extends Actor {
+  def receive: Receive = LoggingReceive {
+    case Lobby.Connected(client) =>
+      println(s"$client connected...")
+      if (client == self)
+        println("That's me!")
+    case Lobby.Disconnected(client) =>
+      println(s"$client connected...")
+  }
+}
+
+class LobbyApp extends Actor {
+
+  val lobby = context.actorOf(Props[Lobby], "lobby")
+  println(self)
+  println(lobby)
+
+  def receive: Receive = LoggingReceive {
+    case msg => println(msg)
+  }
+
+}
+
+class LobbyClientApp extends Actor {
+
+  val lobby = context.actorSelection("akka.tcp://Main@127.0.0.1:2552/user/app/lobby")
+  val client = context.actorOf(Props[LobbyClient])
+
+  lobby ! Lobby.Connect(client)
+
+  def receive: Receive = LoggingReceive {
+    case msg => println(msg)
+  }
+  
+}
